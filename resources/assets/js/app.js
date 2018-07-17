9cc6ff3b469ddf25a27a46e0dcb4c3da22209eee
NaisOne <wlanforfree@gmail.com>
2018-07-17 12:52:50 +0200

    Include vuex-store and routersync in in app

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import { sync } from 'vuex-router-sync'
+import VuexStore from './vuex/store'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 library.add(faCoffee)
+
+Vue.use(Vuex);
+const store = new Vuex.Store(VuexStore);
+sync(store, router);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
